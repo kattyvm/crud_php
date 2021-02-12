c0f16089c1ec2bb6d72cce81d455b2133f409723
katty <a20160555@pucp.edu.pe>
2021-02-11 20:07:38 -0500

    commit

--- web/db.php
+++ web/db.php
@@ -3,13 +3,19 @@
 session_start();
 
 $host="34.71.71.133";
-		$port=3306;
-		$socket="";
-		$user="root";
-		$password="katty123";
-		$dbname="test";
+$port=3306;
+$socket="";
+$user="root";
+$password="katty123";
+$dbname="test";
 
-		$con = new mysqli($host, $user, $password, $dbname, $port, $socket)
+//$host=getenv('MYSQL_HOST');
+//$user=getenv('MYSQL_DB');
+//$password=getenv('MYSQL_USER');
+//$dbname=getenv('MYSQL_PASS');
+//$port=getenv('MYSQL_PORT');
+
+$con = new mysqli($host, $user, $password, $dbname, $port, $socket) 
 		or die ('Could not connect to the database server' . mysqli_connect_error());
 
 //$con->close();
